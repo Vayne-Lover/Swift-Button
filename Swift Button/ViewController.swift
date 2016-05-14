7aea9300fec5b5d928a752bfc380c662142bf9b6
Vayne-Lover <406378362@qq.com>
2016-05-14 10:41:41 +0800

    Initial Commit

---
+++ Swift Button/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Button
+//
+//  Created by 姜况童 on 16/5/14.
+//  Copyright © 2016年 姜况童. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
